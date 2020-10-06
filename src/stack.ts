2b585a288279a77cca9ced6f227f8d8505291095
Kane Zhu <me@kane.mx>
2020-10-06 12:21:03 +0800

    tests: fix unit test.

---
+++ src/stack.ts
@@ -0,0 +1,116 @@
+import * as asing from '@aws-cdk/aws-autoscaling';
+import * as certmgr from '@aws-cdk/aws-certificatemanager';
+import * as ec2 from '@aws-cdk/aws-ec2';
+import * as elb from '@aws-cdk/aws-elasticloadbalancingv2';
+import * as iam from '@aws-cdk/aws-iam';
+import * as r53 from '@aws-cdk/aws-route53';
+import * as r53tg from '@aws-cdk/aws-route53-targets';
+import { Construct, Stack, StackProps, CfnOutput, Duration } from '@aws-cdk/core';
+
+interface DemoStackProps extends StackProps {
+  zoneId?: string;
+  zoneName?: string;
+  acm?: string;
+}
+
+export class DemoStack extends Stack {
+  constructor(scope: Construct, id: string, props?: DemoStackProps) {
+    super(scope, id, props);
+    const userData = ec2.UserData.forLinux();
+    userData.addCommands(`
+set -xe
+exec > >(tee /var/log/user-data.log | logger -t user-data -s 2>/dev/console) 2>&1
+yum update -y
+yum install docker -y
+systemctl start docker
+systemctl enable docker
+sleep 5
+docker run -d -p 80:80 guanyebo/demohttpd:v1
+systemctl status amazon-ssm-agent
+systemctl enable amazon-ssm-agent
+systemctl restart amazon-ssm-agent
+exit 0`);
+    // define resources here...
+    const vpc = new ec2.Vpc(this, 'newVpc', {
+      maxAzs: 2,
+      natGateways: 1,
+    });
+    const acmArn = props?.acm ?? this.node.tryGetContext('acm');
+    if (!acmArn) {
+      throw new Error('ACM ARN is required.');
+    }
+    const acm = certmgr.Certificate.fromCertificateArn(this, 'demoAcm', acmArn);
+    const alb = new elb.ApplicationLoadBalancer(this, 'myalb', {
+      vpc,
+      internetFacing: true,
+      loadBalancerName: 'demoalb',
+    } );
+    const asg = new asing.AutoScalingGroup(this, 'webASG', {
+      vpc,
+      instanceType: new ec2.InstanceType('t2.micro'),
+      machineImage: ec2.MachineImage.latestAmazonLinux(
+        { generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2 }),
+      desiredCapacity: 3,
+      spotPrice: '0.0104',
+      blockDevices: [
+        {
+          deviceName: '/dev/xvda',
+          volume: asing.BlockDeviceVolume.ebs(30),
+        },
+      ],
+      userData,
+    });
+    asg.addToRolePolicy(
+      new iam.PolicyStatement({
+        actions: [
+          'ssmmessages:*',
+          'ssm:UpdateInstanceInformation',
+          'ec2messages:*',
+        ],
+        resources: ['*'],
+      }));
+    asg.connections.allowFrom( ec2.Peer.ipv4('0.0.0.0/0'), ec2.Port.tcp(80));
+    alb.addListener('myWebhttp', {
+      port: 80,
+      open: true,
+      defaultAction: elb.ListenerAction.redirect( {
+        protocol: 'HTTPS',
+        host: '#{host}',
+        path: '/#{path}',
+        query: '/#{query}',
+        port: '443',
+      }),
+    });
+    const httpslistener = alb.addListener('myWebhttps', {
+      certificates: [acm],
+      port: 443,
+      open: true,
+    });
+    httpslistener.addTargets('webServer', {
+      port: 80,
+      targets: [asg],
+    });
+    const zoneId = props?.zoneId ?? this.node.tryGetContext('zoneId');
+    this.node.addWarning(`Zone id is ${zoneId}`);
+    if (!zoneId) {
+      throw new Error('ZoneId is required.');
+    }
+    const zoneName = props?.zoneName ?? this.node.tryGetContext('zoneName');
+    if (!zoneName) {
+      throw new Error('ZoneName is required.');
+    }
+    const zone = r53.HostedZone.fromHostedZoneAttributes(this, 'myZone', {
+      hostedZoneId: zoneId,
+      zoneName: zoneName,
+    });
+    const r53alias = new r53.ARecord(this, 'alias-alb', {
+      zone,
+      target: r53.RecordTarget.fromAlias(new r53tg.LoadBalancerTarget(alb)),
+      recordName: 'cdkdemo',
+      ttl: Duration.minutes(5),
+    });
+    new CfnOutput(this, 'aliasalbOutput', {
+      value: r53alias.domainName,
+    });
+  }
+}
--- test/main.test.ts
+++ test/main.test.ts
@@ -1,6 +1,6 @@
 import { App } from '@aws-cdk/core';
 import '@aws-cdk/assert/jest';
-import { DemoStack } from '../src/main';
+import { DemoStack } from '../src/stack';
 
 const devEnv = {
   account: '1234567890xx',
@@ -8,13 +8,16 @@ const devEnv = {
 };
 
 test('Snapshot', () => {
-  const app = new App();
+  const app = new App({
+    context: {
+      acm: 'arn:aws:acm:region:account-id:certificate/zzzzzzz-2222-3333-4444-3edc4rfv5t',
+      zoneId: 'XXXXXXXXXXXXX',
+      zoneName: 'example.com',
+    },
+  });
   const stack = new DemoStack(app, 'testing', {
     env: devEnv,
   } );
-  stack.node.setContext('acm', 'arn:aws:acm:region:account-id:certificate/zzzzzzz-2222-3333-4444-3edc4rfv5t' );
-  stack.node.setContext('zoneId', 'XXXXXXXXXXXXX' );
-  stack.node.setContext('zoneName', 'example.com' );
   expect(stack).not.toHaveResource('AWS::S3::Bucket');
   expect(stack).toHaveResource('AWS::EC2::Instance', {
     BlockDeviceMappings: [
